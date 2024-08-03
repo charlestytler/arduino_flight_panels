01cadadd0c487554d5910d085ae368967207db8d
Charlie Tytler <cjtytler@gmail.com>
2024-08-03 00:05:46 -0700

    Working left_console.ino

---
+++ left_console/GpioExpander.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <Joystick.h>
+#include <PCF8574.h>
+
+enum GpioType {
+    S, // Single pole input
+    D, // Double pole input
+    M, // Multiple pole (e.g. rotary switch) input
+    L, // LED output
+    X  // Unused
+};
+
+typedef struct {
+    uint8_t pcf_address; // Expected as binary of form 0b<A0><A1><A2>
+    GpioType pins[8];
+    int joystick_start_index;
+} GpioConfig;
+
+class GpioExpander
+{
+  public:
+    GpioExpander() : gpio_board_() {}
+
+    void set_configuration(GpioConfig &config)
+    {
+        uint8_t pcf_id = 0b100000 | config.pcf_address;
+#ifdef DEBUG
+        Serial.print("PCF ID: ");
+        Serial.print(pcf_id, BIN);
+        Serial.print("\t");
+        Serial.println(pcf_id, HEX);
+#endif
+        gpio_board_.setAddress(pcf_id);
+
+        /*
+        Button mask for PCF8574 library:
+        Bits set to 1 will set those pins HIGH then LOW to read button switch status.
+        Bits set to 0 will be unaffected during the buttonRead8() call (good for LED outputs).
+        */
+        constexpr int NUM_PINS = 8;
+        uint8_t button_mask = 0U;
+        for (int i = 0; i < NUM_PINS; i++) {
+            if (config.pins[i] == S || config.pins[i] == D || config.pins[i] == M) {
+                bitWrite(button_mask, i, 1);
+            }
+        }
+        gpio_board_.setButtonMask(button_mask);
+#ifdef DEBUG
+        Serial.print("Mask: ");
+        Serial.println(button_mask, BIN);
+#endif
+
+        joystick_start_index_ = config.joystick_start_index;
+    }
+
+    void set_led(const uint8_t pin_id, const uint8_t value) { gpio_board_.write(pin_id, value); }
+
+    void set_all_led(const uint8_t value) { gpio_board_.write8(0); }
+
+    void setup(GpioConfig &config)
+    {
+        set_configuration(config);
+        gpio_board_.begin();
+    }
+
+    void loop(Joystick_ &joystick)
+    {
+        uint8_t pin_values = gpio_board_.readButton8();
+#ifdef DEBUG
+        Serial.print("Board ");
+        Serial.print(gpio_board_.getAddress(), HEX);
+        Serial.print("  Values: ");
+        Serial.println(pin_values, BIN);
+#endif
+
+        for (int i = 0; i < 8; i++) {
+            if (bitRead(gpio_board_.getButtonMask(), i)) {
+                joystick.setButton(joystick_start_index_ + i, !(bitRead(pin_values, i)));
+            }
+        }
+    }
+
+  private:
+    PCF8574 gpio_board_;
+    int joystick_start_index_;
+};
--- left_console/left_console.ino
+++ left_console/left_console.ino
@@ -1,9 +1,10 @@
-
 #define DCSBIOS_DEFAULT_SERIAL
 
+// #define DEBUG
+
+#include "GpioExpander.h"
 #include <DcsBios.h>
 #include <Joystick.h>
-#include "../GpioExpansion.h"
 
 const uint8_t hidReportId = 100;
 const uint8_t buttonCount = 80;
@@ -28,8 +29,7 @@ Joystick_ joystick(hidReportId,
 
 int btn_index = 0;
 
-enum GpioExpanderNames
-{
+enum GpioExpanderNames {
     LDG_GEAR_1 = 0,
     LDG_GEAR_2,
     FLT_CONTROL,
@@ -39,24 +39,18 @@ enum GpioExpanderNames
     FUEL_2,
     AFCS,
     NUM_GPIO_EXPANDERS // Maximum value of enum
-}
-
-GpioExpander io[] = {
-    // LDG_GEAR
-    GpioExpander(0, 0, 0, {L, L, L, L, X, X, X, X}, btn_index),
-    GpioExpander(1, 0, 0, {D, D, S, S, S, S, S, S}, btn_index += 8),
-    // FLT_CONTROL
-    GpioExpander(0, 1, 1, {L, S, S, S, S, S, S, S}, btn_index += 8),
-    // ENG_START
-    GpioExpander(0, 1, 0, {D, D, D, D, S, X, X, L}, btn_index += 8),
-    // REFUEL
-    GpioExpander(0, 0, 1, {D, D, S, S, S, S, S, X}, btn_index += 8),
-    // FUEL
-    GpioExpander(1, 0, 1, {S, S, S, S, D, D, D, D}, btn_index += 8),
-    GpioExpander(1, 1, 0, {D, D, S, D, D, S, X, X}, btn_index += 8),
-    // AFCS
-    GpioExpander(1, 1, 1, {D, D, D, D, S, D, D, X}, btn_index += 8),
 };
+
+GpioExpander io[NUM_GPIO_EXPANDERS];
+GpioConfig io_configs[NUM_GPIO_EXPANDERS] = { //
+    [LDG_GEAR_1] = {0b000, {L, L, L, L, X, X, X, X}, btn_index},
+    [LDG_GEAR_2] = {0b100, {D, D, S, S, S, S, S, S}, btn_index += 8},
+    [FLT_CONTROL] = {0b011, {L, S, S, S, S, S, S, S}, btn_index += 8},
+    [ENG_START] = {0b010, {D, D, D, D, S, L, D, D}, btn_index += 8},
+    [REFUEL] = {0b001, {D, D, S, S, S, S, S, X}, btn_index += 8},
+    [FUEL_1] = {0b101, {S, S, S, S, D, D, D, D}, btn_index += 8},
+    [FUEL_2] = {0b110, {D, D, S, D, D, S, X, X}, btn_index += 8},
+    [AFCS] = {0b111, {D, D, D, D, S, D, D, X}, btn_index += 8}};
 
 // Callback functions for specific LEDs.
 void set_ldg_gear_handle_led(int value) { io[LDG_GEAR_1].set_led(0, value); }
@@ -69,42 +63,64 @@ void set_eng_start_apu_led(int value) { io[ENG_START].set_led(7, value); }
 
 void turn_off_all_leds()
 {
-    for (int i = 0; i < NUM_GPIO_EXPANDERS)
-    {
+    for (int i = 0; i < NUM_GPIO_EXPANDERS; i++) {
         io[i].set_all_led(0);
     }
 }
 
 // DCS BIOS Configuration
 
-void onAcftNameChange(char *newValue)
-{
-    turn_off_all_leds();
-}
+void onAcftNameChange(char *newValue) { turn_off_all_leds(); }
 DcsBios::StringBuffer<24> AcftNameBuffer(0x0000, onAcftNameChange);
 
-DcsBios::IntegerBuffer A10LdgGearHandle(0x747e, 0x0800, 99, set_ldg_gear_handle_led);
-DcsBios::IntegerBuffer A10LdgGearLeft(0x7430, 0x1000, 99, set_ldg_gear_left_led);
-DcsBios::IntegerBuffer A10LdgGearRight(0x7430, 0x2000, 99, set_ldg_gear_right_led);
-DcsBios::IntegerBuffer A10LdgGearNose(0x7430, 0x0800, 99, set_ldg_gear_nose_led);
+DcsBios::IntegerBuffer A10LdgGearHandle(0x1026, 0x4000, 14, set_ldg_gear_handle_led);
+DcsBios::IntegerBuffer A10LdgGearLeft(0x1026, 0x1000, 12, set_ldg_gear_left_led);
+DcsBios::IntegerBuffer A10LdgGearRight(0x1026, 0x2000, 13, set_ldg_gear_right_led);
+DcsBios::IntegerBuffer A10LdgGearNose(0x1026, 0x0800, 11, set_ldg_gear_nose_led);
 
+DcsBios::IntegerBuffer F18LdgGearHandle(0x747e, 0x0800, 11, set_ldg_gear_handle_led);
+DcsBios::IntegerBuffer F18LdgGearLeft(0x7430, 0x1000, 12, set_ldg_gear_left_led);
+DcsBios::IntegerBuffer F18LdgGearRight(0x7430, 0x2000, 13, set_ldg_gear_right_led);
+DcsBios::IntegerBuffer F18LdgGearNose(0x7430, 0x0800, 11, set_ldg_gear_nose_led);
+//
+DcsBios::IntegerBuffer F18ApuReady(0x74c2, 0x0800, 11, set_eng_start_apu_led);
+
+DcsBios::IntegerBuffer F16LdgGearHandle(0x447c, 0x0002, 1, set_ldg_gear_handle_led);
+DcsBios::IntegerBuffer F16LdgGearLeft(0x447a, 0x8000, 15, set_ldg_gear_left_led);
+DcsBios::IntegerBuffer F16LdgGearRight(0x447c, 0x0001, 0, set_ldg_gear_right_led);
+DcsBios::IntegerBuffer F16LdgGearNose(0x447a, 0x4000, 14, set_ldg_gear_nose_led);
 
 // Arduino Runtime
 
-void setup() {
+void setup()
+{
+#ifdef DEBUG
+    Serial.begin(115200);
+    delay(5000);
+    Serial.println("START");
+#endif
+
     DcsBios::setup();
     constexpr bool autoSendState = false;
     joystick.begin(autoSendState);
+
+    Wire.begin();
+    for (int i = 0; i < NUM_GPIO_EXPANDERS; i++) {
+        delay(1000);
+        io[i].setup(io_configs[i]);
+    }
+    turn_off_all_leds();
 }
 
-constexpr int LOOP_FRAME_TIME_MS = 10;
-void loop() {
+constexpr int LOOP_FRAME_TIME_MS = 10; // Run at 100Hz.
+void loop()
+{
     unsigned long frame_start_time_ms = millis();
 
-    for (int i=0; i<NUM_GPIO_EXPANDERS; i++) {
-        io[i].loop(&joystick);
+    for (int i = 0; i < NUM_GPIO_EXPANDERS; i++) {
+        io[i].loop(joystick);
     }
-
+    joystick.setButton(1, 1);
 
     joystick.sendState();
 
