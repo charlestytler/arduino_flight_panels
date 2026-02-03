3ee8143c1e6dcf32d8a92559feae80be228a4fef
Charles Tytler <cjtytler@gmail.com>
2026-02-03 13:38:08 -0800

    fix config initialization in right_console

--- sketches/right_console/right_console.ino
+++ sketches/right_console/right_console.ino
@@ -1,21 +1,16 @@
 #include <FlightSimBreakoutBoard.h>
 
-
 constexpr int NUM_GPIO_EXPANDERS = 1;
-const uint8_t expander_i2c_addresses[NUM_GPIO_EXPANDERS] = {0x20};
 constexpr int NUM_LEDS = 0;
-const DigitalIOExpandersConfig digital_io_expanders_config = {expander_i2c_addresses, NUM_GPIO_EXPANDERS, NUM_LEDS};
+const DigitalIOExpandersConfig digital_io_expanders_config = {
+    {0x20}, // expander_i2c_addresses
+    NUM_GPIO_EXPANDERS,
+    NUM_LEDS};
 
 const FlightSimBreakoutBoardConfig config = {digital_io_expanders_config};
 
 FlightSimBreakoutBoard flightSimBreakoutBoard(config);
 
-void setup()
-{
-  flightSimBreakoutBoard.setup(config);
-}
+void setup() { flightSimBreakoutBoard.setup(config); }
 
-void loop()
-{
-  flightSimBreakoutBoard.loop();
-}
+void loop() { flightSimBreakoutBoard.loop(); }
