37c68c6d897f61370281a5bb0d90ca36f69b8c05
Charles Tytler <cjtytler@gmail.com>
2026-02-05 22:41:06 -0800

    Add static assert checks

--- sketches/right_console/right_console.ino
+++ sketches/right_console/right_console.ino
@@ -1,16 +1,55 @@
 #include <FlightSimBreakoutBoard.h>
 
-constexpr int NUM_GPIO_EXPANDERS = 1;
-constexpr int NUM_LEDS = 0;
-const DigitalIOExpandersConfig digital_io_expanders_config = {
-    {0x20}, // expander_i2c_addresses
-    NUM_GPIO_EXPANDERS,
-    NUM_LEDS};
+constexpr FlightSimBreakoutBoardConfig config{
+    .digital_io_expanders_config =
+        {
+            .expander_i2c_addresses = {0x20},
+            .num_expanders = 1, // num_expanders
+            .num_leds = 0,      // num_leds
+        },
+    .analog_multiplexer_config = {
+        .pins = {.s0 = 15, .s1 = 14, .s2 = 16, .s3 = 10}, // Channel Select pins
+        .num_mux_inputs = 4,
+    }};
 
-const FlightSimBreakoutBoardConfig config = {digital_io_expanders_config};
+// const FlightSimBreakoutBoardConfig config{
+//     {
+//         // digital_io_expanders_config
+//         {0x20},
+//         1, // num_expanders
+//         0, // num_leds
+//     },
+//     {
+//         // analog_multiplexer_config
+//         {A0, A1, A2, A3}, // Channel Select pins
+//         4,                // num_mux_inputs
+//     }};
 
 FlightSimBreakoutBoard flightSimBreakoutBoard(config);
 
-void setup() { flightSimBreakoutBoard.setup(config); }
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+  }
+
+  flightSimBreakoutBoard.setup(config);
+}
 
 void loop() { flightSimBreakoutBoard.loop(); }
+
+//
+//
+// Checks on configuration.
+
+static_assert(config.digital_io_expanders_config.num_leds <=
+                  NUM_PINS_PER_IO_EXPANDER *
+                      config.digital_io_expanders_config.num_expanders,
+              "More LEDs than available pins.");
+
+static_assert(config.digital_io_expanders_config.num_expanders <=
+                  MAX_NUM_DIGITAL_IO_EXPANDERS,
+              "More expanders than number of I2C addresses available.");
+
+static_assert(config.analog_multiplexer_config.num_mux_inputs <=
+                  MAX_NUM_MULTIPLEXERS,
+              "More mux inputs than max configuration supports.");
